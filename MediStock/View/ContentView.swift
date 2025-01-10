24a78ed8ef11d0e00db4a0d422538577b72fd244
Modibo KEITA <modykeita2@gmail.com>
2025-01-10 16:24:17 +0100

    change update viewModel

--- MediStock/View/ContentView.swift
+++ MediStock/View/ContentView.swift
@@ -2,23 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var session: SessionStore
-    @StateObject var authViewModel = AuthViewModel()
+    @StateObject var authViewModel : AuthViewModel
     var body: some View {
-        Group {
-            if session.session != nil {
-                MainTabView()
-            } else {
-                LoginView()
+        VStack {
+            Group {
+                if authViewModel.session != nil {
+                    MainTabView()
+                } else {
+                    LoginView()
+                }
             }
+            .onAppear
+            {
+                session.listen()
         }
-        .onAppear {
-            session.listen()
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environmentObject(SessionStore())
+        ContentView(authViewModel: AuthViewModel(user: User(uid: "Info"))).environmentObject(SessionStore())
     }
 }
--- MediStock/ViewModel/AuthViewModel.swift
+++ MediStock/ViewModel/AuthViewModel.swift
@@ -12,19 +12,19 @@ class AuthViewModel : ObservableObject {
     @Published var messageError : String = ""
     @Published var user: User?
 
-    init(session : SessionStore = SessionStore(), user: User? =  SessionStore().session){
+    init(session : SessionStore = SessionStore(), user: User?){
         self.session = session
     }
     
     func login(email:String, password:String){
         session.signIn(email: email, password: password)
         self.messageError = session.messageError
+        self.user = session.session
     }
     
     func createdNewUser(email: String, password: String){
         session.signUp(email: email, password: password)
         self.messageError = session.messageError
     }
-    
     
 }
