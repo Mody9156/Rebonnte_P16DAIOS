528ff13de03429d45bacd919e4ff1767ef81dfac
Modibo KEITA <modykeita2@gmail.com>
2025-01-11 21:42:25 +0100

    update viewModel

--- MediStock/ViewModel/ViewModelManager.swift
+++ MediStock/ViewModel/ViewModelManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class ViewModelManager:ObservableObject {
+    @Published var isAuthenticated : Bool
+
+    init(){
+        self.isAuthenticated = false
+    }
+    
+    var authViewModel : AuthViewModel {
+        return AuthViewModel { [weak self] in
+            self?.isAuthenticated = true
+        }
+    }
+    
+}
