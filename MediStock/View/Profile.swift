5acb92f463ce5661bd7032f1033626a6b4984338
Modibo KEITA <modykeita2@gmail.com>
2025-01-15 18:04:16 +0100

    update text

--- MediStock/View/Profile.swift
+++ MediStock/View/Profile.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct Profile: View {
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Profile")
+            
         }
     }
 }
