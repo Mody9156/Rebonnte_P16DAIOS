9f0b9271d7130af32b4ad9f5ebaa9fcd4ea65e27
Modibo KEITA <modykeita2@gmail.com>
2025-01-16 13:14:57 +0100

    add order

--- MediStock/Model/Service/MedicineRepository.swift
+++ MediStock/Model/Service/MedicineRepository.swift
@@ -28,14 +28,14 @@ class MedicineRepository: ObservableObject {
     }
     
     func fetchAisles(completion:@escaping( [String])->Void) {
-        db.collection("medicines").addSnapshotListener { (querySnapshot, error) in
+        db.collection("medicines").order(by: "aisle", descending: true).addSnapshotListener { (querySnapshot, error) in
             if let error = error {
                 print("Error getting documents: \(error)")
             } else {
                 let allMedicines = querySnapshot?.documents.compactMap { document in
                     try? document.data(as: Medicine.self)
                 } ?? []
-                let aisles = Array(Set(allMedicines.map { $0.aisle })).sorted(by: <)
+                let aisles = Array(Set(allMedicines.map { $0.aisle })).sorted()
                 completion(aisles)
             }
         }
