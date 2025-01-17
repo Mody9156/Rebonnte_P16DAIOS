c162552c04df6454faa0905dbf52a2d5fa984406
Modibo KEITA <modykeita2@gmail.com>
2025-01-17 12:39:06 +0100

    use aisle

--- MediStock/View/MedicineListView.swift
+++ MediStock/View/MedicineListView.swift
@@ -19,7 +19,7 @@ struct MedicineListView: View {
         }
         .navigationBarItems(trailing: Button(action: {
             Task{
-                try await viewModel.addRandomMedicine(user: "test_user") // Remplacez par l'utilisateur actuel
+                try await viewModel.addRandomMedicine(user: aisle) // Remplacez par l'utilisateur actuel
             }
         }) {
             Image(systemName: "plus")
