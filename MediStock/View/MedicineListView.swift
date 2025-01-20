ac2f748fbde16a23bc22bebf2697fd9a46a4c1e4
Modibo KEITA <modykeita2@gmail.com>
2025-01-20 16:26:46 +0100

    delete filter

--- MediStock/View/MedicineListView.swift
+++ MediStock/View/MedicineListView.swift
@@ -7,9 +7,7 @@ struct MedicineListView: View {
     var body: some View {
         VStack {
             List {
-                ForEach(viewModel.medicines.filter({ Medicine in
-                    Medicine.aisle == aisle
-                }), id: \.id) { medicine in
+                ForEach(viewModel.medicines, id: \.id) { medicine in
                     NavigationLink(destination: MedicineDetailView(medicine: medicine)) {
                         VStack(alignment: .leading) {
                             Text(medicine.name)
