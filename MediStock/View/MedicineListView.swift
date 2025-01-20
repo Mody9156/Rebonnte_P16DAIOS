6f72a107ddfdc310d8fa583b9a053a81c9c6bf23
Modibo KEITA <modykeita2@gmail.com>
2025-01-20 17:29:13 +0100

    add new variable

--- MediStock/View/MedicineListView.swift
+++ MediStock/View/MedicineListView.swift
@@ -4,6 +4,7 @@ struct MedicineListView: View {
     @ObservedObject var viewModel = MedicineStockViewModel()
     var aisle: String
     @State private var email = UserDefaults.standard.string(forKey: "email")
+    @Environment(\.dismiss) var dismiss
 
     var filterMedicines : [Medicine] {
         return viewModel.medicines.filter({ Medicine in
@@ -12,32 +13,33 @@ struct MedicineListView: View {
     }
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(filterMedicines, id: \.id) { medicine in
-                    NavigationLink(destination: MedicineDetailView(medicine: medicine)) {
-                        VStack(alignment: .leading) {
-                            Text(medicine.name)
-                                .font(.headline)
-                            Text("Stock: \(medicine.stock)")
-                                .font(.subheadline)
-                        }
+        List {
+            ForEach(filterMedicines, id: \.id) { medicine in
+                NavigationLink(destination: MedicineDetailView(medicine: medicine)) {
+                    VStack(alignment: .leading) {
+                        Text(medicine.name)
+                            .font(.headline)
+                        Text("Stock: \(medicine.stock)")
+                            .font(.subheadline)
                     }
                 }
-                .onDelete { IndexSet in
-                    viewModel.deleteMedicines(at: IndexSet)
-                }
             }
-            .navigationBarItems(trailing:Button(action: {
-                Task{
-                    guard let email else {return}
-                    try await viewModel.addRandomMedicineToList(user: email, aisle: aisle) // Remplacez par l'utilisateur actuel
-                }
-            }) {
-                Image(systemName: "plus")
-            })
-            .navigationBarTitle(aisle)
+            .onDelete { IndexSet in
+                viewModel.deleteMedicines(at: IndexSet)
+                dismiss()
+            }
         }
+        .navigationBarItems(trailing:Button(action: {
+            Task{
+                guard let email else {return}
+                try await viewModel.addRandomMedicineToList(user: email, aisle: aisle) // Remplacez par l'utilisateur actuel
+                dismiss()
+            }
+        }) {
+            Image(systemName: "plus")
+        })
+        .navigationBarTitle(aisle)
+        
         .onAppear {
             viewModel.observeMedicines()
         }
