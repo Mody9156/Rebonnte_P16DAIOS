13331813b9bd4b745cff2c1996eeaf585b518992
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 17:36:42 +0100

    update class with right values

--- MediStock/Model/Service/Auth/FirebaseAuthService.swift
+++ MediStock/Model/Service/Auth/FirebaseAuthService.swift
@@ -6,7 +6,36 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
 
 class FirebaseAuthService: AuthServiceProtocol {
+    func signUp(email: String, password: String) async throws -> User {
+        let result = try await Auth.auth().createUser(withEmail: email, password: password)
+        return  User(uid: result.user.uid, email: result.user.email)
+    }
     
+    func signIn(email: String, password: String) async throws -> User {
+        let result = try await Auth.auth().signIn(withEmail: email, password: password)
+        return User(uid: result.user.uid, email: result.user.email)
+    }
+    
+    func signOut() async throws {
+        try Auth.auth().signOut()
+    }
+    
+    func addDidChangeListenerHandle(listener: @escaping (User?) -> Void) {
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                listener(User(uid: user.uid,email: user.email))
+            }else{
+                listener(nil)
+            }
+        }
+    }
+    
+    func removeDidChangeListenerHandle(handle: AuthStateDidChangeListenerHandle) {
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
 }
