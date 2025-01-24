749b6533305370c82921f4358f35ca4916dd5417
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 20:54:17 +0100

    add comment

--- MediStockTests/Model/SessionStoreTests.swift
+++ MediStockTests/Model/SessionStoreTests.swift
@@ -7,14 +7,8 @@
 
 import XCTest
 @testable import pack
-import Foundation
-import Firebase
-import FirebaseAuth
-import FirebaseFirestore
-import FirebaseCore
 
 final class SessionStoreTests: XCTestCase {
     
-    var sessionStore : SessionStore
 
 }
