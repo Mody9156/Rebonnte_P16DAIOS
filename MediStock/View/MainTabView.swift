b06780808c959c58ce8c16222cbe651e8343290a
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 12:47:26 +0100

    use accesibility

--- MediStock/View/MainTabView.swift
+++ MediStock/View/MainTabView.swift
@@ -6,26 +6,39 @@ struct MainTabView: View {
             AisleListView()
                 .tabItem {
                     Image(systemName: "list.dash")
+                        .accessibilityLabel("Aisles List Icon")
                     Text("Aisles")
+
                 }
-            
+                .accessibilityLabel("Aisles")
+                .accessibilityHint("View the list of aisles in the pharmacy.")
+
             AllMedicinesView()
                 .tabItem {
                     Image(systemName: "square.grid.2x2")
+                        .accessibilityLabel("Medicines Grid Icon")
                     Text("All Medicines")
                 }
+                .accessibilityLabel("All Medicines")
+                .accessibilityHint("View the list of all medicines available.")
             
             Profile(authViewModel: AuthViewModel(), use: User(uid: ""))
                 .tabItem {
                     Image(systemName: "person.crop.circle.fill")
+                        .accessibilityLabel("Profile Icon")
                     Text("Profile")
                 }
+                .accessibilityLabel("Profile")
+                .accessibilityHint("View and edit your profile information.")
+
         }
+        .accessibilityLabel("Main Tab Bar")
+        .accessibilityHint("Use the tabs to switch between different sections of the app.")
     }
 }
-//
-//struct MainTabView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MainTabView(medicines: Medicine()
-//    }
-//}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView(medicines: Medicine()
+    }
+}
