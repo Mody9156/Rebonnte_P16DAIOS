a9e79bd05bec6dc6f0b4324ac3ba0b5b9e64740b
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 12:48:04 +0100

    dalete comment

--- MediStock/View/MainTabView.swift
+++ MediStock/View/MainTabView.swift
@@ -39,6 +39,6 @@ struct MainTabView: View {
 
 struct MainTabView_Previews: PreviewProvider {
     static var previews: some View {
-        MainTabView(medicines: Medicine()
+        MainTabView()
     }
 }
