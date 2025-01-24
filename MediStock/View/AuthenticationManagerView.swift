2f41b666302bc5dfa4fa6a68175555dbfe583be2
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 12:18:34 +0100

    add accessibility

--- MediStock/View/AuthenticationManagerView.swift
+++ MediStock/View/AuthenticationManagerView.swift
@@ -8,8 +8,15 @@ struct AuthenticationManagerView: View {
             Group {
                 if authViewModel.isAuthenticated {
                     MainTabView()
+                        .accessibilityLabel("Main Application View")
+                        .accessibilityHint("Navigates to the main application content.")
+
                 } else {
                     LoginView()
+                        .accessibilityLabel("Login Scrren")
+                        .accessibilityHint("Allows you to log in to your account.")
+
+
                 }
             }
             .onAppear{
@@ -23,11 +30,14 @@ struct AuthenticationManagerView: View {
                 authViewModel.stopListeningToAuthChanges()
             }
         }
+        .accessibilityElement(children: .contain) // Groups children as one accessibility element if needed
+        .accessibilityLabel("Authentication Manager")
+        .accessibilityHint("Determines if the user is logged in or needs to log in.")
     }
 }
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView().environmentObject(SessionStore())
-//    }
-//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationManagerView().environmentObject(SessionStore())
+    }
+}
