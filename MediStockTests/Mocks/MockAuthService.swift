3981c9ee354925fe3b694f5227624b125099d68a
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 20:22:48 +0100

    initialsie mock

--- MediStockTests/Mocks/MockAuthService.swift
+++ MediStockTests/Mocks/MockAuthService.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import pack
 
-class MockAuthService{
-    
+class MockAuthService: AuthServiceProtocol {
+    var mockUser: User?
+    var shouldThrowError: Bool = false
+
+    func signUp(email: String, password: String) async throws -> User {
+        if shouldThrowError { throw AuthError.userCreationFailed }
+        return User(uid: "mockUID", email: email)
+    }
+
+    func signIn(email: String, password: String) async throws -> User {
+        if shouldThrowError { throw AuthError.invalidCredentials }
+        return User(uid: "mockUID", email: email)
+    }
+
+    func signOut() async throws {
+        if shouldThrowError { throw AuthError.unknown }
+    }
+
+    func addStateDidChangeListener(listener: @escaping (User?) -> Void) {
+        listener(mockUser)
+    }
+
+    func removeStateDidChangeListener(handle: AuthStateDidChangeListenerHandle) {}
 }
+
