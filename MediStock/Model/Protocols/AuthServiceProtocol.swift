7283258feddeeaf55b4f50cdbaa33bcc84e03eb8
Modibo KEITA <modykeita2@gmail.com>
2025-01-24 13:56:38 +0100

    add new folder

--- MediStock/Model/Protocols/AuthServiceProtocol.swift
+++ MediStock/Model/Protocols/AuthServiceProtocol.swift
@@ -6,7 +6,15 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
 
 protocol AuthServiceProtocol {
-    <#requirements#>
+    func signUp(email: String, password: String) async throws -> User
+    func signIn(email: String, password: String) async throws -> User
+    func signOut() async throws
+    func addDidChangeListenerHandle(listener: @escaping (User?) -> Void)
+    func removeDidChangeListenerHandle(handle : AuthStateDidChangeListenerHandle)
 }
