fa9e428356f8810918bada9f8afe33e1832cec7e
Modibo KEITA <modykeita2@gmail.com>
2025-01-27 10:39:55 +0100

    use new protocol

--- MediStockTests/Mocks/MockAuthService.swift
+++ MediStockTests/Mocks/MockAuthService.swift
@@ -12,7 +12,22 @@ import FirebaseFirestore
 import FirebaseCore
 import FirebaseAuth
 
+protocol UserProtocol {
+    
+}
 
+class MockUser : Users {
+    private let emailValue: String?
+
+     override var email: String? {
+         return emailValue
+     }
+
+     init(email: String?) {
+         self.emailValue = email
+         super.init() // Firebase User ne fournit pas de constructeur public, ceci est fictif
+     }
+}
 
 
 class MockAuthService: AuthServiceProtocol {
