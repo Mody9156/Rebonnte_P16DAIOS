374c6b9f3c64cf14720674a47a495a5f221e5d35
Modibo KEITA <modykeita2@gmail.com>
2025-01-29 16:58:03 +0100

    replace las protocol MockQueryDocumentSnapshotProtocol

--- MediStockTests/Mocks/MockMedicineService.swift
+++ MediStockTests/Mocks/MockMedicineService.swift
@@ -4,11 +4,34 @@
 //
 //  Created by KEITA on 29/01/2025.
 //
-
 import XCTest
 @testable import pack
 import FirebaseFirestore
 
-final class MockMedicineService: Firestore {
-  
+// MARK: - Mock Medicine Service
+final class MockMedicineService {
+ 
+}
+
+
+class MockCollectionReferenceProtocol{
+    
+}
+
+class MockQuerySnapshotProtocol{
+    
+}
+
+class MockQueryDocumentSnapshotProtocol: QueryDocumentSnapshotProtocol{
+    private var mockData : [String: Any]
+    var documentID: String
+    
+    init(mockData: [String : Any], documentID: String) {
+        self.mockData = mockData
+        self.documentID = documentID
+    }
+    
+    func data() -> [String : Any] {
+        return mockData
+    }
 }
