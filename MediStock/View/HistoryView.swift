953ba1c2b3f4bf88b8af41675ae5c1f05c06128f
Modibo KEITA <modykeita2@gmail.com>
2025-02-03 13:24:08 +0100

    use ScrollView

--- MediStock/View/HistoryView.swift
+++ MediStock/View/HistoryView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct HistoryView: View {
     var body: some View {
-        Text("History")
-    }
+        ScrollView {
+            VStack {
+                ForEach(filterMedicine) { entry in
+                    VStack(alignment: .leading,spacing: 5) {
+                        Text(entry.action)
+                            .font(.headline)
+                            .accessibilityLabel("Action: \(entry.action)")
+                        
+                        Text("User: \(entry.user)")
+                            .font(.subheadline)
+                            .accessibilityLabel("Performed by: \(entry.user)")
+                        
+                        Text("Date: \(entry.timestamp.formatted())")
+                            .font(.subheadline)
+                            .accessibilityLabel("Date: \(entry.timestamp.formatted())")
+                        
+                        Text("Details: \(entry.details)")
+                            .font(.subheadline)
+                            .accessibilityLabel("Details: \(entry.details)")
+                        
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.bottom, 5)
+                    .accessibilityElement(children: .combine)
+                    .accessibilityHint("Details of this history entry.")
+                }
+            }
+        }    }
 }
 
 struct HistoryView_Previews: PreviewProvider {
