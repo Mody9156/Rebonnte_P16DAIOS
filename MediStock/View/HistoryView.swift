e8c0b90ea2188fcd46966bc4dc2040b0b609fa05
Modibo KEITA <modykeita2@gmail.com>
2025-02-03 14:46:27 +0100

    add ZStack

--- MediStock/View/HistoryView.swift
+++ MediStock/View/HistoryView.swift
@@ -10,48 +10,47 @@ import SwiftUI
 struct HistoryView: View {
     var filterMedicine : [HistoryEntry]
     var count: Int {
-      return filterMedicine.count - 1
+        return filterMedicine.count - 1
     }
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(filterMedicine) { entry in
-                    HStack {
-                        VStack {
-                            Image(systemName: "circle.fill")
-                                .foregroundColor(.blue)
+        ZStack {
+            ScrollView {
+                VStack {
+                    ForEach(filterMedicine) { entry in
+                        HStack {
+                            VStack {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
                                 HStack {
                                     Divider()
                                         .frame(width:4)
                                         .overlay(.blue)
+                                }
                             }
-                        }
-                        
-                        VStack(alignment: .leading,spacing: 5) {
-                            Text(entry.action)
-                                .font(.headline)
-                                .accessibilityLabel("Action: \(entry.action)")
                             
-                            HStack {
+                            VStack(alignment: .leading,spacing: 5) {
+                                Text(entry.action)
+                                    .font(.headline)
+                                    .accessibilityLabel("Action: \(entry.action)")
+                                
                                 Text("User: \(entry.user)")
                                     .font(.subheadline)
-                                .accessibilityLabel("Performed by: \(entry.user)")
+                                    .accessibilityLabel("Performed by: \(entry.user)")
                                 Text("Date: \(entry.timestamp.formatted())")
                                     .font(.subheadline)
                                     .accessibilityLabel("Date: \(entry.timestamp.formatted())")
+                                
+                                Text("Details: \(entry.details)")
+                                    .font(.subheadline)
+                                    .accessibilityLabel("Details: \(entry.details)")
                             }
+                            .padding()
+                            .cornerRadius(10)
+                            .padding(.bottom, 5)
+                            .accessibilityElement(children: .combine)
+                            .accessibilityHint("Details of this history entry.")
                             
-                            Text("Details: \(entry.details)")
-                                .font(.subheadline)
-                                .accessibilityLabel("Details: \(entry.details)")
                         }
-                        .padding()
-                        .cornerRadius(10)
-                        .padding(.bottom, 5)
-                        .accessibilityElement(children: .combine)
-                        .accessibilityHint("Details of this history entry.")
-                        
-                       
                     }
                 }
             }
@@ -62,6 +61,6 @@ struct HistoryView: View {
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
         HistoryView(filterMedicine: [HistoryEntry(medicineId: "48f949df", user: "James@gmail.com", action: "Update", details: "change stock with new values", timestamp: Date.now),
-        HistoryEntry(medicineId: "48f949df", user: "James@gmail.com", action: "Update", details: "change stock with new values", timestamp: Date.now)])
+                                     HistoryEntry(medicineId: "48f949df", user: "James@gmail.com", action: "Update", details: "change stock with new values", timestamp: Date.now)])
     }
 }
