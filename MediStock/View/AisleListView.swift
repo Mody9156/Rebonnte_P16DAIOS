b016624eaf9d06ec4101ca663b92c9cb967a3df7
KEITA Modibo <Modykeita2@gmail.com>
2025-02-17 09:54:26 +0100

    delete function

--- MediStock/View/AisleListView.swift
+++ MediStock/View/AisleListView.swift
@@ -30,7 +30,6 @@ struct AisleListView: View {
                             }
                         }
                     }
-                    
                 }
                 
                 Button(action: {
