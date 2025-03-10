d981bfdea013392773bc3cf496a987c9043d24dc
KEITA Modibo <Modykeita2@gmail.com>
2025-03-10 08:50:39 +0100

    update NavigationBarTitle

--- MediStock/View/AisleListView.swift
+++ MediStock/View/AisleListView.swift
@@ -39,7 +39,7 @@ struct AisleListView: View {
                             }
                         }
                     }
-                    .navigationBarTitle("")
+                    .navigationBarTitle("Aisle")
                 }
                 
                 Button(action: {
