d13dabcaab074eb6c0f5ba0b98c38407830d1ae4
KEITA Modibo <Modykeita2@gmail.com>
2025-03-13 18:10:00 +0100

    add button

--- MediStock/View/AddANewAisle.swift
+++ MediStock/View/AddANewAisle.swift
@@ -7,35 +7,124 @@
 
 import SwiftUI
 
-enum AisleList: String, CaseIterable, Identifiable {
-    case Analgesics, Antibiotics, Antibacterials
-    var id: Self { self }
+enum SelectAisleList: String, CaseIterable, Identifiable {
+    
+    case Analgesics = "Analgesics and Anti-inflammatory drugs"
+    case Antibiotics = "Antibiotics and Antibacterials"
+    case Antituberculosi = "Antituberculosis drugs"
+    
+    var id: String  { self.rawValue }
+}
+
+enum SelectNameOfMedicineList: String, CaseIterable, Identifiable {
+    
+    case Peripherally = "Peripherally acting analgesics"
+    case Centrally = "Centrally acting analgesics"
+    case NonSteroidal = "Non-Steroidal Anti-Inflammatory Drugs (NSAIDs)"
+    case Steroidal = "Steroidal Anti-Inflammatory Drugs (Corticosteroids)"
+    
+    var id: String  { self.rawValue }
+}
+
+enum SelectNameOfMedicineListTwo: String, CaseIterable, Identifiable {
+    
+    case Beta = "Beta-lactams"
+    case Aminoglycosides = "Aminoglycosides"
+    case Macrolides = "Macrolides and related compounds"
+    case Antibiotics = "Antibiotics with retained antistaphylococcal activity"
+    case Tetracyclines = "Tetracyclines (Cyclines)"
+    case casePhenicols = "casePhenicols"
+    case Cyclic = "Cyclic polypeptides (Polymyxins)"
+    case Sulfonamides = "Sulfonamides and Diaminopyrimidines"
+    case Quinolones = "Quinolones"
+    case Nitroimidazole = "Nitroimidazole derivatives"
+    case Miscellaneous = "Miscellaneous antibiotics"
+    case antibacterials =  "antibacterials"
+    
+    var id: String  { self.rawValue }
 }
 
 struct AddANewAisle: View {
-    @State private var iterateAisles : [String] = ["Analgesics and Anti-inflammatory drugs","Antibiotics and Antibacterials"]
+    @State private var iterate : SelectAisleList = SelectAisleList.allCases.first!
     @State private var stock : [Int] = Array(0...100)
-    @State private var name : [String] = ["Peripherally acting analgesics","Centrally acting analgesics","Antispasmodics","Non-Steroidal Anti-Inflammatory Drugs (NSAIDs)","Steroidal Anti-Inflammatory Drugs (Corticosteroids)"]
-    
+    @State private var stockSelected : Int = 0
+    @State private var selectNameOfMedicineList : SelectNameOfMedicineList = SelectNameOfMedicineList.allCases.first!
+    @State private var nameInAisle : String = ""
+    @State private var selectNameOfMedicineListTwo : SelectNameOfMedicineListTwo = .allCases.first!
+    @State private var nameInAisleMEdicine : String = ""
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Select Aisles")
-                Picker("Aisles", selection: $iterateAisles) {
-                    ForEach(iterateAisles,id: \.self) { flavor in
-                        Text(flavor)
+        ZStack {
+            Color(.gray)
+                .ignoresSafeArea()
+                .opacity(0.1)
+            
+            Circle()
+                .frame(height: 200)
+                .position(x: 1, y: 1)
+                .foregroundStyle(.blue)
+                .opacity(0.4)
+            
+            Circle()
+                .frame(height: 200)
+                .position(x: 400, y: 800)
+                .foregroundStyle(.blue)
+                .opacity(0.4)
+            
+            ScrollView {
+                VStack {
+                    
+                    Text("Selected Aisle: \(nameInAisle)")
+                    Text("Selected Medication: \(nameInAisleMEdicine)")
+                    Text("Stock : \(stockSelected)")
+                    
+                    Text("Aisle")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    
+                    Picker("Aisles", selection: $nameInAisle) {
+                        ForEach(SelectAisleList.allCases) { aisle in
+                            Text(aisle.rawValue.capitalized)
+                        }
                     }
-                }.pickerStyle(.wheel)
-                
-                Text("Stock")
-                    .padding()
-                Picker("Aisles", selection: $stock) {
-                    ForEach(stock,id: \.self){ number in
-                        Text("\(number)")
+                    .pickerStyle(.menu)
+                    
+                    Text("Stock")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    
+                    Picker("Aisles", selection: $stockSelected) {
+                        ForEach(stock,id: \.self){ number in
+                            Text("\(number)")
+                        }
+                    }.pickerStyle(.menu)
+                    
+                    Text("Medicine")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    
+                    Picker("Medicine", selection: $nameInAisleMEdicine) {
+                        ForEach(SelectNameOfMedicineList.allCases) { name in
+                            Text(name.rawValue.capitalized)
+                        }
+                    }.pickerStyle(.menu)
+                    
+                    Picker("Medicine", selection: $nameInAisleMEdicine) {
+                        ForEach(SelectNameOfMedicineListTwo.allCases) { name in
+                            Text(name.rawValue.capitalized)
+                        }
+                    }.pickerStyle(.menu)
+                    
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Validate")
+                        .foregroundStyle(.white)
+                        .background(Color.blue)
+                        .frame(height: 40)
                     }
-                }.pickerStyle(.inline)
-                
-                Text("Medicine")
+                }
+                .padding()
             }
         }
     }
