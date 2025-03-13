37c9eadd696c93337d90e9cda55f6207200f6093
KEITA Modibo <Modykeita2@gmail.com>
2025-03-13 13:08:26 +0100

    add new state

--- MediStock/View/AddANewAisle.swift
+++ MediStock/View/AddANewAisle.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
+enum AisleList: String, CaseIterable, Identifiable {
+    case Analgesics, Antibiotics, Antibacterials
+    var id: Self { self }
+}
+
 struct AddANewAisle: View {
-    @State private var iterateAisles : [String] = ["Analgesics and Anti-inflammatory drugs"]
+    @State private var iterateAisles : [String] = ["Analgesics and Anti-inflammatory drugs","Antibiotics and Antibacterials"]
+    @State private var stock : [Int] = Array(0...100)
+    @State private var name : [String] = ["Peripherally acting analgesics","Centrally acting analgesics","Antispasmodics","Non-Steroidal Anti-Inflammatory Drugs (NSAIDs)","Steroidal Anti-Inflammatory Drugs (Corticosteroids)"]
+    
     var body: some View {
-        Text("Hello, World!")
-        Picker("", selection: $iterateAisles) {
-            
+        ScrollView {
+            VStack {
+                Text("Select Aisles")
+                Picker("Aisles", selection: $iterateAisles) {
+                    ForEach(iterateAisles,id: \.self) { flavor in
+                        Text(flavor)
+                    }
+                }.pickerStyle(.wheel)
+                
+                Text("Stock")
+                    .padding()
+                Picker("Aisles", selection: $stock) {
+                    ForEach(stock,id: \.self){ number in
+                        Text("\(number)")
+                    }
+                }.pickerStyle(.inline)
+                
+                Text("Medicine")
+            }
         }
     }
 }
