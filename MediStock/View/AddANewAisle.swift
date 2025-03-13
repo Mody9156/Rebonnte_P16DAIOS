fd21c3ab9aa9c8002f779cc13720b98484501247
KEITA Modibo <Modykeita2@gmail.com>
2025-03-13 11:56:40 +0100

    add new variable

--- MediStock/View/AddANewAisle.swift
+++ MediStock/View/AddANewAisle.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct AddANewAisle: View {
+    var iterateAisles : [String] = ["Analgesics and Anti-inflammatory drugs"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            
     }
 }
 
