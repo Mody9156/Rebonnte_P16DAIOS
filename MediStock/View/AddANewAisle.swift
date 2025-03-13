a757ec0e94053765efc8a213217cc4a3f823868d
KEITA Modibo <Modykeita2@gmail.com>
2025-03-13 11:20:02 +0100

    add a new view

---
+++ MediStock/View/AddANewAisle.swift
@@ -0,0 +1,18 @@
+//
+//  AddANewAisle.swift
+//  pack
+//
+//  Created by Modibo on 13/03/2025.
+//
+
+import SwiftUI
+
+struct AddANewAisle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddANewAisle()
+}
