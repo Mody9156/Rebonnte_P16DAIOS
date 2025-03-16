77813ecfbf859b019d696e3584606317de17baf9
KEITA Modibo <Modykeita2@gmail.com>
2025-03-16 16:35:22 +0100

    update state for binding

--- MediStock/View/LoginView.swift
+++ MediStock/View/LoginView.swift
@@ -4,7 +4,7 @@ struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
     @StateObject private var authViewModel = AuthViewModel()
-    @State private var selectedAutoConnection : Bool = false
+    @Binding private var selectedAutoConnection : Bool 
     
     var body: some View {
         ZStack {
