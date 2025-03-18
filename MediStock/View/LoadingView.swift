9d933d2e4553ad1c65994a6692ef05afb7b67b76
KEITA Modibo <Modykeita2@gmail.com>
2025-03-18 17:59:03 +0100

    add vstack

--- MediStock/View/LoadingView.swift
+++ MediStock/View/LoadingView.swift
@@ -11,7 +11,9 @@ struct LoadingView: View {
     @State var selectedAutoConnection: Bool = UserDefaults.standard.bool(forKey: "autoLogin")
     @State private var loeadView : Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Await")
+        }
     }
 }
 
