cf7338427ff0bb0e0331ac7259efebb173f3b69d
KEITA Modibo <Modykeita2@gmail.com>
2025-03-18 17:54:23 +0100

    add new state

--- MediStock/View/LoadingView.swift
+++ MediStock/View/LoadingView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var loeadView : Bool = false 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
