73c1367d587ef0306f27924578d05650e9850825
KEITA Modibo <Modykeita2@gmail.com>
2025-03-18 17:53:38 +0100

    add new view

---
+++ MediStock/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  pack
+//
+//  Created by Modibo on 18/03/2025.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
