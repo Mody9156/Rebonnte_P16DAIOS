949d30736ded3e8d7d81f1fb5051679f55194465
KEITA Modibo <Modykeita2@gmail.com>
2025-03-19 14:57:16 +0100

    refactoring

--- MediStock/View/AddANewAisle.swift
+++ MediStock/View/AddANewAisle.swift
@@ -16,7 +16,7 @@ struct AddANewAisle: View {
     @State var isEditing : Bool = false
     @ObservedObject var medicineStockViewModel = MedicineStockViewModel()
     @AppStorage("toggleDarkMode") private var toggleDarkMode : Bool = false
-    @State private var selectedAisles : String  = ""
+    @State private var selectedMedicines : String  = ""
     @State private var selectedCategory : String = ""
 
     var body: some View {
