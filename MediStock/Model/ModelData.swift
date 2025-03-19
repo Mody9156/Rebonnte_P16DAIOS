0a1a2e23fb8f3bda5420b7b07054cc932586ad4c
KEITA Modibo <Modykeita2@gmail.com>
2025-03-19 10:59:37 +0100

    add new struct

--- MediStock/Model/ModelData.swift
+++ MediStock/Model/ModelData.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct ModelData {
+    static func chargement<T: Decodable>(_ nomFichier: String) -> T {//fonctions génériques
+        let data: Data
+        
+        guard let file = Bundle.main.url(forResource: nomFichier, withExtension: nil)
+        else {
+            fatalError("Impossible de trouver \(nomFichier) dans le main bundle.")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Impossible de charger \(nomFichier) depuis le main bundle:\n\(error)")
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Impossible de parser \(nomFichier) en tant que \(T.self):\n\(error)")
+        }
+    }
+}
