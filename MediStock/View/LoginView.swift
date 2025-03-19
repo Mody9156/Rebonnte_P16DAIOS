210691639068153014002a33ead7cda16451b2fe
KEITA Modibo <Modykeita2@gmail.com>
2025-03-19 16:41:59 +0100

    custom view

--- MediStock/View/LoginView.swift
+++ MediStock/View/LoginView.swift
@@ -4,8 +4,8 @@ struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
     @StateObject private var authViewModel = AuthViewModel()
-    @Binding  var selectedAutoConnection : Bool
-    @AppStorage("toggleDarkMode") private var toggleDarkMode : Bool = false
+    @Binding var selectedAutoConnection: Bool
+    @AppStorage("toggleDarkMode") private var toggleDarkMode: Bool = false
     
     var body: some View {
         ZStack {
@@ -13,22 +13,23 @@ struct LoginView: View {
                 .ignoresSafeArea()
                 .opacity(0.2)
             
-            Text("Sign In")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.blue)
-                .frame(maxWidth: .infinity, alignment: .center) // Centrer le titre
-            
-            
             VStack(alignment: .leading) {
-                ZStack{
+                
+                
+                Text("Sign In")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                
+                ZStack {
                     RoundedRectangle(cornerRadius: 20)
-                        .fill(Color("BackgroundButton"))
+                        .fill(Color("BackgroundGreen"))
                         .opacity(0.8)
-                        .frame(height: 250)
+                        .frame(height: 200)
                     
-                    VStack(spacing:15){
-                        VStack(alignment: .leading, spacing: 5){
+                    VStack(spacing: 15) {
+                        VStack(alignment: .leading, spacing: 5) {
                             Text("Email")
                                 .foregroundColor(.white)
                                 .padding(.leading, 10)
@@ -38,7 +39,7 @@ struct LoginView: View {
                                 .padding(.horizontal, 10)
                         }
                         
-                        VStack(alignment: .leading, spacing: 5){
+                        VStack(alignment: .leading, spacing: 5) {
                             Text("Password")
                                 .foregroundColor(.white)
                                 .padding(.leading, 10)
@@ -57,46 +58,43 @@ struct LoginView: View {
                             selectedAutoConnection.toggle()
                             authViewModel.saveAutoConnectionState(selectedAutoConnection)
                         }
-                        
                     } label: {
                         ZStack {
                             Rectangle()
                                 .frame(width: 20, height: 20)
                                 .foregroundStyle(.white)
-                                .border(.blue, width: 2)
+                                .border(.green, width: 2)
                                 .cornerRadius(2)
                             
                             if selectedAutoConnection {
-                                Image(systemName: "xmark")
-                                    .foregroundColor(.blue)
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.green)
                                     .font(.system(size: 14, weight: .bold))
                             }
                         }
                     }
+                    .padding(.leading)
                     
                     Text("Stay signed in")
-                        .foregroundColor(.blue)
+                        .foregroundColor(.green)
                         .font(.system(size: 16))
                         .padding(.leading, 5)
                 }
-                .padding(.horizontal,20)
+                .padding()
                 
-                ButtonForUpdateSession(email: $email, password: $password, text:"Login")
-                ButtonForUpdateSession(email: $email, password: $password, text:"Sign Up")
-                
+                ButtonForUpdateSession(email: $email, password: $password, text: "Login")
+                ButtonForUpdateSession(email: $email, password: $password, text: "Sign Up")
             }
             .padding()
-            
         }
         .preferredColorScheme(toggleDarkMode ? .dark : .light)
     }
 }
 
-#Preview{
+#Preview {
     struct PreviewWrapper: View {
-        @State var selected : Bool = false
-        var body: some View
-        {
+        @State var selected: Bool = false
+        var body: some View {
             LoginView(selectedAutoConnection: $selected).environmentObject(SessionStore())
         }
     }
@@ -104,19 +102,19 @@ struct LoginView: View {
 }
 
 struct ButtonForUpdateSession: View {
-    @Binding var email : String
-    @Binding var password : String
+    @Binding var email: String
+    @Binding var password: String
     @StateObject var authViewModel = AuthViewModel()
     @State private var visible: Bool = false
-    var text : String
+    var text: String
     
     var body: some View {
-        VStack(spacing: 8){
+        VStack(spacing: 8) {
             Button(action: {
-                Task{
+                Task {
                     if text == "Login" {
                         try await authViewModel.login(email: email, password: password)
-                    }else{
+                    } else {
                         try await authViewModel.createdNewUser(email: email, password: password)
                     }
                     
@@ -125,17 +123,18 @@ struct ButtonForUpdateSession: View {
                 }
             }) {
                 Text(text)
-                    .foregroundColor(text == "Login" ? .white : .blue)
+                    .foregroundColor(text == "Login" ? .white : .green)
                     .frame(maxWidth: .infinity, minHeight: 50)
-                    .background(text == "Login" ? Color("BackgroundButton") : Color.clear)
+                    .background(text == "Login" ? Color("BackgroundGreen") : Color.clear) 
                     .cornerRadius(12)
                     .overlay(
                         RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color("BackgroundButton"), lineWidth: 2))
+                            .stroke(Color("BackgroundGreen"), lineWidth: 2)
+                    )
             }
             .padding(.horizontal, 20)
             
-            if visible && !authViewModel.messageError.isEmpty{
+            if visible && !authViewModel.messageError.isEmpty {
                 Text(authViewModel.messageError)
                     .foregroundColor(.red)
                     .font(.headline)
@@ -144,18 +143,17 @@ struct ButtonForUpdateSession: View {
                     .animation(.easeOut(duration: 0.7), value: visible)
             }
         }
-        .onChange(of: authViewModel.messageError){ newValue in
+        .onChange(of: authViewModel.messageError) { newValue in
             visible = !newValue.isEmpty
             ThorowsMessagesError()
         }
     }
     
-    private func ThorowsMessagesError(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3 ){
+    private func ThorowsMessagesError() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             withAnimation {
                 visible = false
             }
-            
         }
     }
 }
