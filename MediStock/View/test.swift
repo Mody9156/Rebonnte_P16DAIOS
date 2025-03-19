dcf86bcebf10d9c402a8fdf14df06f48baa80b37
KEITA Modibo <Modykeita2@gmail.com>
2025-03-19 11:32:20 +0100

    add new view

---
+++ MediStock/View/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  pack
+//
+//  Created by Modibo on 19/03/2025.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
