146160fc6af4bf4f77ab524f157022f2cef946ba
KEITA Modibo <Modykeita2@gmail.com>
2025-03-21 18:01:46 +0100

    add new mock

--- MediStockTests/Mocks/MockAuthViewModel.swift
+++ MediStockTests/Mocks/MockAuthViewModel.swift
@@ -8,7 +8,38 @@
 import Testing
 @testable import pack
 
-struct MockAuthViewModel {
+struct MockAuthViewModel : AuthViewModelProtocol{
+    
+    var isAuthenticated: Bool
+    
+    var messageError: String
+    
+    var onLoginSucceed: (() -> Void)?
+    
+    func login(email: String, password: String) async throws {
+        <#code#>
+    }
+    
+    func createdNewUser(email: String, password: String) async throws {
+        <#code#>
+    }
+    
+    func changeStatus() async throws {
+        <#code#>
+    }
+    
+    func disableAutoLogin() async throws {
+        <#code#>
+    }
+    
+    func saveAutoConnectionState(_ state: Bool) {
+        <#code#>
+    }
+    
+    func autotoLogin() async throws {
+        <#code#>
+    }
+    
 
 
 }
