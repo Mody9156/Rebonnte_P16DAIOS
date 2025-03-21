2aa7c2be48ec411f580797fe8ac1ef0a9b038208
KEITA Modibo <Modykeita2@gmail.com>
2025-03-21 14:47:20 +0100

    delete text

--- MediStock/View/AddANewAisle.swift
+++ MediStock/View/AddANewAisle.swift
@@ -19,13 +19,7 @@ struct AddANewAisle: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                
                 VStack(spacing: 20) {
-                    Text("Add a New Aisle")
-                        .font(.title)
-                        .bold()
-                        .padding(.top, 20)
-                    
                     VStack(alignment: .leading, spacing: 8) {
                         Text("Aisle")
                             .font(.headline)
