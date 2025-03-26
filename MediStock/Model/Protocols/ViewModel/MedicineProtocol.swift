03acf6d09cfd53803ecd84764e43633e831aa0ba
KEITA Modibo <Modykeita2@gmail.com>
2025-03-26 10:20:35 +0100

    initialise protocol

--- MediStock/Model/Protocols/ViewModel/MedicineProtocol.swift
+++ MediStock/Model/Protocols/ViewModel/MedicineProtocol.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol MedicineProtocol {
+    func fetchMedicines() async throws -> [Medicine]
+    func fetchAisles() async throws -> [String]
+    func setDataToList(user: String, name: String, stock: Int, aisle: String, stockValue: Int) async throws -> [Medicine]
+    func setDataToAisle(name: String, stock: Int, aisle: String) async throws -> [Medicine]
+    func delete(medicines: [Medicine], at offsets: IndexSet) async throws
+    func deleteAisle(aisles: [String], at offsets: IndexSet) async throws -> [String]
+    func addHistory(action: String, user: String, medicineId: String, details: String, stock: Int) async throws
+    func updateMedicine(_ medicine: Medicine, user: String, stock: Int) async throws
+    func updateStock(_ medicine: Medicine, by amount: Int, user: String, stock: Int) async throws -> [Medicine]
+    func fetchHistory(for medicine: Medicine, completion: @escaping ([HistoryEntry]) -> Void)
+    func trieByName(completion: @escaping ([Medicine]) -> Void)
+    func trieByStock(completion: @escaping ([Medicine]) -> Void)
+    func getAllElements(completion: @escaping ([Medicine]) -> Void)
+}
