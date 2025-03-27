09f271eb39875bd76fdc9400b87e7ad0b065e10f
KEITA Modibo <Modykeita2@gmail.com>
2025-03-27 08:03:35 +0100

    add onDismiss

--- MediStock/View/AisleListView.swift
+++ MediStock/View/AisleListView.swift
@@ -53,7 +53,11 @@ struct AisleListView: View {
                             .foregroundColor(.white)
                     }
                 }
-                .sheet(isPresented: $activeView) {
+                .sheet(isPresented: $activeView, onDismiss: {
+                    Task{
+                        try await medicineStockViewModel.observeAisles()
+                    }
+                }) {
                     AddANewAisle()
                 }
                 .padding()
